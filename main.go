da0578442e047c72b1f14e31dd67470a57ad619e
Maxwell Fusco <54746239+1nv8rzim@users.noreply.github.com>
2023-09-09 18:01:17 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"context"
+)
+
+func Run(ctx context.Context, target string, command string, expectedOutput string, username string, password string) (bool, string) {
+	return true, ""
+}
